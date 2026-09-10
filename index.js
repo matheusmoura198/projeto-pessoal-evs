c6e8d80036aa2a580f9fe328d2a3627d21a54dbb
Theus.moura <mateus.leitao6@aluno.ce.gov.br>
2026-09-10 12:54:34 -0300

    Add multiple conditional checks for answers

---
+++ index.js
@@ -0,0 +1,39 @@
+// //
+const resposta="a"
+if( === "a"){
+  console.log(resposta correta)
+}else{
+  console.log(resposta errada)
+}
+
+// //
+const resposta="b"
+if( === "c"){
+  console.log(resposta correta)
+}else{
+  console.log(respsota errada)
+}
+
+// //
+const resposta="d"
+if( === "d"){
+  console.log(resposta correta)
+}else{
+  console.log(resposta errada)
+}
+
+// //
+const resposta="c"
+if( === "c"){
+  console.log(resposta correta)
+}else{
+  console.log(resposta errada)
+}
+
+// //
+const resposta="e"
+if( === "a"){
+  console.log(resposta correta)
+}else{
+  console.log(resposta errada)
+}
